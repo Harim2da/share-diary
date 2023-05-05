24622b0c2eb6ab315115f3c1cf0c57b15b1e8bdb
seongju <gch03915@naver.com>
2023-05-05 20:41:28 +0900

    feat(230505) : Repository create

---
+++ src/main/java/share_diary/diray/emoji/domain/EmojiRepository.java
@@ -0,0 +1,6 @@
+package share_diary.diray.emoji.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmojiRepository extends JpaRepository<Emoji,Long> {
+}
---
+++ src/main/java/share_diary/diray/member/domain/MemberRespository.java
@@ -0,0 +1,6 @@
+package share_diary.diray.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRespository extends JpaRepository<Member,Long> {
+}
