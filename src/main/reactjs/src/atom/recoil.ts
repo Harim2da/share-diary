804faabe1c9d02b48833372be6101629c20cf718
nyoung03 <vnfdlvdk37@gmail.com>
2023-05-08 20:48:47 +0900

    메뉴 오픈 boolean atom 추가

--- src/main/reactjs/src/atom/recoil.ts
+++ src/main/reactjs/src/atom/recoil.ts
@@ -4,3 +4,8 @@ export const selectDateState = atom<string>({
   key: "selectDateState",
   default: "",
 });
+
+export const isMenuOpenState = atom<boolean>({
+  key: "isMenuOpenState",
+  default: false,
+});
