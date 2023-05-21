726099b9743f4214796823ebf1f9fee9f71a9508
SeongJu Lee <gch03915@naver.com>
2023-05-21 21:04:31 +0900

    20230521 : MemberNotFound 에러 수정

--- src/main/java/share_diary/diray/exception/member/MemberNotFoundException.java
+++ src/main/java/share_diary/diray/exception/member/MemberNotFoundException.java
@@ -2,6 +2,6 @@ package share_diary.diray.exception.member;
 
 import share_diary.diray.exception.http.NotFoundException;
 
-public class MemberNotFound extends NotFoundException {
+public class MemberNotFoundException extends NotFoundException {
 
 }
