33e84c44186e7a440ff7614454621594a694075e
SeongJu Lee <gch03915@naver.com>
2023-05-29 21:36:13 +0900

    feat() : Role enum 추가 + 연관관계 설정 및 편의메소드 추가

--- src/main/java/share_diary/diray/emoji/domain/Emoji.java
+++ src/main/java/share_diary/diray/emoji/domain/Emoji.java
@@ -1,13 +1,13 @@
 package share_diary.diray.emoji.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import share_diary.diray.dailyDiary.domain.DailyDiary;
+import share_diary.diray.member.domain.Member;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -39,4 +39,47 @@ public class Emoji {
     private int cakeEmojiNumber;
 
     private int devilEmojiNumber;
+
+    //회원
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    //일기
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="daily_diary_id")
+    private DailyDiary dailyDiary;
+
+    @Builder
+    public Emoji(int angryEmojiNumber, int badEmojiNumber, int boringEmojiNumber, int goodEmojiNumber, int refreshEmojiNumber, int funEmojiNumber, int heartEmojiNumber, int thumbSupEmojiNumber, int partyPopperEmojiNumber, int cakeEmojiNumber, int devilEmojiNumber) {
+        this.angryEmojiNumber = angryEmojiNumber;
+        this.badEmojiNumber = badEmojiNumber;
+        this.boringEmojiNumber = boringEmojiNumber;
+        this.goodEmojiNumber = goodEmojiNumber;
+        this.refreshEmojiNumber = refreshEmojiNumber;
+        this.funEmojiNumber = funEmojiNumber;
+        this.heartEmojiNumber = heartEmojiNumber;
+        this.thumbSupEmojiNumber = thumbSupEmojiNumber;
+        this.partyPopperEmojiNumber = partyPopperEmojiNumber;
+        this.cakeEmojiNumber = cakeEmojiNumber;
+        this.devilEmojiNumber = devilEmojiNumber;
+    }
+
+    //연관관계 편의메소드
+    public void addMember(Member member){
+        if(this.member != null){
+            this.member.getEmojis().remove(this);
+        }
+        this.member = member;
+        member.getEmojis().add(this);
+    }
+
+    //연관관계 편의메소드
+    public void addDailyDiary(DailyDiary dailyDiary){
+        if(this.dailyDiary != null){
+            this.dailyDiary.getEmoji().remove(this);
+        }
+        this.dailyDiary = dailyDiary;
+        dailyDiary.getEmoji().add(this);
+    }
 }
--- src/main/java/share_diary/diray/memberDiaryRoom/domain/MemberDiaryRoom.java
+++ src/main/java/share_diary/diray/memberDiaryRoom/domain/MemberDiaryRoom.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import share_diary.diray.diaryRoom.DiaryRoom;
+import share_diary.diray.member.domain.Member;
 
 import javax.persistence.*;
 
@@ -16,7 +17,42 @@ public class MemberDiaryRoom {
     @Id @GeneratedValue
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //회원
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //일기방
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "diary_room_id", referencedColumnName = "id")
     private DiaryRoom diaryRoom;
+
+    public MemberDiaryRoom(Role role) {
+        this.role = role;
+    }
+
+    public static MemberDiaryRoom from(Role role){
+        return new MemberDiaryRoom(role);
+    }
+
+    //연관관계 편의 메소드
+    public void addMember(Member member){
+        if(this.member != null){
+            this.member.getMemberDiaryRooms().remove(this);
+        }
+        this.member = member;
+        member.getMemberDiaryRooms().add(this);
+    }
+
+    //연관관계 편의 메소드
+    public void addDiaryRoom(DiaryRoom diaryRoom){
+        if(this.diaryRoom!=null){
+            this.diaryRoom.getMemberDiaryRooms().remove(this);
+        }
+        this.diaryRoom = diaryRoom;
+        diaryRoom.getMemberDiaryRooms().add(this);
+    }
 }
