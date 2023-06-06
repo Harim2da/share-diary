1da2cc91be83ba6240619a56a87317efc0290593
nyoung03 <vnfdlvdk37@gmail.com>
2023-06-06 23:36:47 +0900

    랭킹 페이지 추가

--- src/main/reactjs/src/App.tsx
+++ src/main/reactjs/src/App.tsx
@@ -1,18 +1,21 @@
-import React from "react";
 import { Route, Routes } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./view/Main";
 import WriteDiary from "./view/WriteDiary";
+import Ranking from "./view/Ranking";
 import Login from "./view/Login";
 import SignUp from "./view/SignUp";
 import FindIdPw from "./view/FindIdPw";
+import ResetRecoil from "./component/Common/ResetRecoil";
 
 function App() {
   return (
     <Router>
+      <ResetRecoil />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/write" element={<WriteDiary />} />
+        <Route path="/ranking" element={<Ranking />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/find" element={<FindIdPw />} />
