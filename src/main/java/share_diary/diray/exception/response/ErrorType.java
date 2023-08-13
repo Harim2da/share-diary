4ce2633778c0eee1b5769e3d0db1043873ae7e83
SeongJu Lee <gch03915@naver.com>
2023-08-13 15:41:12 +0900

    feat() : redis 에서 인증번호 조회 오류 exception 생성

--- src/main/java/share_diary/diray/exception/response/ErrorType.java
+++ src/main/java/share_diary/diray/exception/response/ErrorType.java
@@ -2,6 +2,7 @@ package share_diary.diray.exception.response;
 
 import lombok.Getter;
 import share_diary.diray.exception.BaseException;
+import share_diary.diray.exception.certification.CertificationNotFoundException;
 import share_diary.diray.exception.diaryRoom.DiaryRoomNotFoundException;
 import share_diary.diray.exception.jwt.AccessTokenRenewException;
 import share_diary.diray.exception.jwt.TokenExpiredException;
@@ -19,6 +20,8 @@ public enum ErrorType {
     A001("A001","토큰이 유효하지 않습니다.", TokenIsNotValidException.class),
     A002("A002","토큰이 만료되었습니다.", TokenExpiredException.class),
     A003("A003","ACCESS TOKEN 이 만료되었습니다.", AccessTokenRenewException.class),
+
+    C001("C001","인증번호가 유효하지 않습니다.", CertificationNotFoundException.class),
 
     M001("M001","회원을 찾을 수 없습니다.", MemberNotFoundException.class),
     M002("M002","아이디 혹은 비밀번호가 올바르지 않습니다.",MemberIdOrPasswordErrorException.class),
