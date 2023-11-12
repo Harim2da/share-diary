c2da6dc3e21dd5844f10e09fa41c6620945c2bd9
이하림 <helloharim09@gmail.com>
2023-11-12 21:05:16 +0900

    [신규] 선택한 날짜의 일기 조회

---
+++ src/main/java/share_diary/diray/exception/dailyDiary/TooManyDailyDiariesException.java
@@ -0,0 +1,7 @@
+package share_diary.diray.exception.dailyDiary;
+
+import share_diary.diray.exception.http.InternalServerException;
+
+public class TooManyDailyDiariesException extends InternalServerException {
+
+}
