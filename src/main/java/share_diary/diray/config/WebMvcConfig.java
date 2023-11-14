3277531d9ee9d06f63d502efa27cd67c4e931c7b
SeongJu Lee <gch03915@naver.com>
2023-11-14 21:45:26 +0900

    feat() : cors 허용

--- src/main/java/share_diary/diray/config/WebMvcConfig.java
+++ src/main/java/share_diary/diray/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package share_diary.diray.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import share_diary.diray.auth.AuthService;
@@ -28,5 +29,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthenticationPrincipalArgumentResolver(authService));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
     }
 }
